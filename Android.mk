70c8431f5ba9b54f57c0116f527cc4f26d0996f9
bigbiff bigbiff <bigbiff@teamw.in>
2017-03-31 06:38:43 -0400

    Fix codename.

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 ifeq (RECOVERY_VARIANT), twrp)
-ifeq ($(TARGET_DEVICE), zara)
+ifeq ($(TARGET_DEVICE), kanas3gnfc)
 
 LOCAL_PATH := $(call my-dir)
 include $(call first-makefiles-under, $(LOCAL_PATH))
